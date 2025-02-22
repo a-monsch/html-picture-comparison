fe1742465eedcfb59770a780ff13b96ceabf6174
Artur Monsch <60860160+a-monsch@users.noreply.github.com>
2025-02-22 22:15:35 +0100

    Adding first version of darknode

--- js/globalButtonFunctionality.js
+++ js/globalButtonFunctionality.js
@@ -100,6 +100,18 @@ document.addEventListener('DOMContentLoaded', () => {
         updateDisplayedPicture();
       });
     }
+
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    if (darkModeToggle) {
+      darkModeToggle.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+        if(document.body.classList.contains('dark-mode')){
+          darkModeToggle.textContent = '🌙';
+        } else {
+          darkModeToggle.textContent = '☀';
+        }
+      });
+    }
     
 });
 
